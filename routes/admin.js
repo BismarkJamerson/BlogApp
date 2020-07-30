16213c28d9b1aa8f1fe4981ff952d82e068cf85f
Bismark Jamerson da Silva <57026193+KrynzaLing@users.noreply.github.com>
2020-07-30 14:35:25 -0300

    Correção bug MongoDB
    Correção bug, erro ao criar/salvar
    Blogapp + categorias
    name="nome"
    name="slug"

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const mongoose = require("mongoose")//Importa Mongoose
+require("../models/Categoria")//Chama arquivo Model
+const Categoria = mongoose.model("categorias")//Referência do model
+
 
 router.get('/',(req, res) => {
     res.render("admin/index")
@@ -13,8 +17,22 @@ router.get('/categorias', (req, res) =>{
     res.render("admin/categorias")
 })
 
-router.get('/categorias/add', (req, res) => {
+router.get('/categorias/add', (req, res) =>{
     res.render("admin/addcategorias")
 })
+
+router.post("/categorias/nova", (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log("Salvo com sucesso!")
+    }).catch((err)=>{
+        console.log("Erro ao salvar ", err)
+    })
+})
+
 
 module.exports = router
