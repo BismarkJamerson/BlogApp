62a44faedc1c2d83a3012489e72f4191a01b43fc
Bismark Jamerson da Silva <57026193+KrynzaLing@users.noreply.github.com>
2020-08-02 15:33:35 -0300

    Middlewares + Sessão
    const session = require("express-session")
    const flash = require("connect-flash")
    
    Configurações: Sessões + Flash + Middlewares

--- app.js
+++ app.js
@@ -6,8 +6,23 @@
     const admin = require("./routes/admin")
     const path = require("path")
     const mongoose = require("mongoose")
+    const session = require("express-session")
+    const flash = require("connect-flash") 
 
 //Configurações
+    //Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())//After session
+        //Middlewares
+            app.use((req, res, next)=>{
+                res.locals.success_msg = req.flash("success_msg")//res.locals = variavel global
+                res.locals.error_msg = req.flash("error_msg")//res.locals = variavel global
+                next()
+            })
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
@@ -22,7 +37,12 @@
             console.log("Erro ao conectar: "+ err)
         })
     //Public
-        app.use(express.static(path.join(__dirname,"public")))        
+        app.use(express.static(path.join(__dirname,"public"))) 
+        //Middlewares
+            app.use((req, res, next)=> {
+                console.log("Middlewares...OK!")
+                next()
+            })       
 //Rotas
     app.use('/admin', admin)
 //Outros
