a45030b6997864aa6a72f6fd5a1ce100c60537a7
Bismark Jamerson da Silva <57026193+KrynzaLing@users.noreply.github.com>
2020-08-03 13:49:15 -0300

    Validação formulario
    Path _msg.handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -22,16 +22,39 @@ router.get('/categorias/add', (req, res) =>{
 })
 
 router.post("/categorias/nova", (req, res) => {
-    const novaCategoria = {
-        nome: req.body.nome,
-        slug: req.body.slug
-    }
+    
+    var erros = []
 
-    new Categoria(novaCategoria).save().then(() => {
-        console.log("Salvo com sucesso!")
-    }).catch((err)=>{
-        console.log("Erro ao salvar ", err)
-    })
+        if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+            erros.push({texto: "Nome inválido"})
+        }
+        if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+            erros.push({texto: "Slug inválido"})
+        }
+
+        if(req.body.nome.length < 2){
+            erros.push({texto: "Nome da categoria muito pequeno"})
+        }
+
+        if(erros.length > 0){
+            res.render("admin/addcategorias", {erros: erros})
+        }else{
+            const novaCategoria = {
+                nome: req.body.nome,
+                slug: req.body.slug
+            }
+        
+            new Categoria(novaCategoria).save().then(() => {
+                req.flash("success_msg", "Categoria criada com sucesso!")
+                console.log("Salvo com sucesso!")
+                res.redirect("/admin/categorias")
+            }).catch((err)=>{
+                req.flash("error_msg", "Erro ao salvar categoria, tente novamente!")
+                console.log("Erro ao salvar ", err)
+                res.redirect("/admin")
+            })
+        }
+     
 })
 
 
