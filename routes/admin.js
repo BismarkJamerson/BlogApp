c452287e6886aeeb84af348b400495dbbaa0dc44
Bismark Jamerson da Silva <57026193+KrynzaLing@users.noreply.github.com>
2020-08-18 23:27:52 -0300

    Deletar categoria
    Metodo não recomendo
    feito para estudo
    
    => metodo anterior é mais seguro

--- routes/admin.js
+++ routes/admin.js
@@ -194,4 +194,14 @@ router.post("/postagem/edit", (req, res)=>{
 
 })
 
+router.get("/postagens/deletar/:id", (req, res)=>{
+    //segunda forma de deletar sem usar formulario
+    Postagem.remove({_id: req.params.id}).then(()=>{
+        req.flash("success_msg", "Deletado com sucesso!")
+        res.redirect("/admin/postagens")
+    }).catch((err)=>{
+        req.flash("error_msg", "Erro ao deletar Postagem!")
+        res.redirect("/admin/postagens")
+    })
+})
 module.exports = router
