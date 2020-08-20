e45068b6ebdb5598aeca937386560e546924a949
Bismark Jamerson da Silva <57026193+KrynzaLing@users.noreply.github.com>
2020-08-20 19:56:06 -0300

    Criando home page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
     const mongoose = require("mongoose")
     const session = require("express-session")
     const flash = require("connect-flash") 
+    require("./models/Postagem")
+    const Postagem = mongoose.model("postagens")
 
 //Configurações
     //Sessão
@@ -44,6 +46,23 @@
                 next()
             })       
 //Rotas
+    app.get('/', (req, res)=>{
+        Postagem.find().lean().populate("categoria").sort({data: "desc"}).then((postagens)=>{
+            res.render("index", {postagens: postagens})
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro")
+            res.redirect("/404")
+        })
+    })
+
+        app.get("/404", (req, res)=>{
+            res.send('Erro 404!')
+        })
+
+    app.get("/posts",(req, res)=>{
+        res.send("Lista Post")
+    })
+
     app.use('/admin', admin)
 //Outros
 const PORT = 8081
